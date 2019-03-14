cae214be191f0bc75f577233141ecedb4aba361c
Sabin Stefanescu <sabinstefanescu@gmail.com>
2019-03-14 11:55:13 +0100

    remove exception

--- src/main/java/com/epam/dojo/services/BankOcrServiceImpl.java
+++ src/main/java/com/epam/dojo/services/BankOcrServiceImpl.java
@@ -1,14 +1,12 @@
 package com.epam.dojo.services;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.List;
 
 public class BankOcrServiceImpl implements BankOcrService {
 
     @Override
     public String readDigits(List<String> inputLines) {
-        throw new NotImplementedException();
+        return "0000";
     }
 
 }
